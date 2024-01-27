70c4f592c98e7a92f88ebdbb01c82392794d168a
mubarak23 <mubarakaminu340@gmail.com>
2024-01-27 13:25:07 +0100

    create Wallet address using P2SH

---
+++ createWalletP2SH.js
@@ -0,0 +1,43 @@
+
+const bitcoin = require('bitcoinjs-lib')
+const ECPairFactory = require('ecpair').default
+const ecc = require('tiny-secp256k1')
+const fs = require('fs');
+
+const EcPair = ECPairFactory(ecc)
+
+const network = bitcoin.networks.testnet
+
+async function createWalletP2SH() {
+  try {
+    const keyPair = EcPair.makeRandom({ network: network})
+   
+    console.log('keyPair.publicKey', keyPair.publicKey)
+   
+    // P2WPKH -> payments.p2wpkh
+
+    const { address } = bitcoin.payments.p2sh({
+      pubkey: keyPair.publicKey,
+      network: network
+    })
+    console.log('address', address)
+
+    const privateKey = keyPair.toWIF()
+    console.log('Private Key', privateKey)
+
+    const wallet = {
+      address: address,
+      privateKey: privateKey
+    }
+    console.log('wallet', wallet)
+
+    const walletJson = JSON.stringify(wallet, null, 4)
+    fs.writeFileSync('createWalletP2SH.json', walletJson)
+    console.log(`createWalletP2SH Wallet created and saved to wallet.json`);
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+createWalletP2SH()
